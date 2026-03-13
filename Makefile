c3c49c75759cdd635ff329a442a67d0c7d2755e7
Chris Muga <ChrisMuga94@Gmail.com>
2026-03-13 22:35:00 +0300

    circa

---
+++ Makefile
@@ -0,0 +1,12 @@
+build:
+	clang -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL libraries/libraylib.a src/main.c -o bin/circa
+	@ echo Build generated at bin/circa
+test:
+	# eval cc src/main.c $(pkg-config --libs --cflags raylib) -o bin/circa
+	# eval cc src/main.c -framework IOKit -framework Cocoa -framework OpenGL $(pkg-config --libs --cflags raylib) -o bin/circa
+run:
+	@ ./bin/circa
+build-run:
+	@ make build
+	@ echo "Running..."
+	@ make run
---
+++ src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "../libraries/raylib/src/raylib.h"
+
+
+int main(void)
+{
+    InitWindow(800, 450, "CIRCA");
+	// TODO: Cannot track the mouse on partial sized displays, i.e non-fullscreen displays
+	// SetWindowSize(1600, 1200);
+
+	// NOTE: Mouse position tracking only works with fullscreen for now.
+	ToggleFullscreen();
+
+	SetTargetFPS(60);
+
+	int MAX = 100;
+
+	Vector2 mousePositions[100] = {0};
+
+	int idx = 0;
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(GRAY);
+			Vector2 MousePosition = GetMousePosition();
+			printf("----> <%f, %f>\n", MousePosition.x, MousePosition.y);
+			// DrawCircle(MousePosition.x, MousePosition.y, 100.0, RED);
+			
+			bool mousePositionDown = IsMouseButtonDown(1);
+			bool mousePositionReleased = IsMouseButtonReleased(1);
+			bool maxObtained = idx == MAX;
+
+
+            DrawText("CIRCA", 190, 200, 60, LIGHTGRAY);
+
+			if(IsMouseButtonPressed(1)){
+				ClearBackground(GRAY);
+			}
+
+			if(idx < MAX){
+				if(MousePosition.x > 0){
+					mousePositions[idx] = MousePosition;
+					printf("IDX --> %d", idx);
+					idx += 1;
+				}
+			}
+
+			if(maxObtained) {
+				DrawText("MAX OBTAINED", 190, 300, 60, LIGHTGRAY);
+
+				for(int i = 0; i < MAX; i++){
+					Vector2 pos = mousePositions[i];
+					int py = 300 + (10 * i+1);
+					// DrawPixelV(pos, BLUE);
+					DrawCircleV(pos, 30.0, RED);
+					Vector2 v = {0, 0};
+					mousePositions[i] =  v;
+				}
+
+				idx = 0;
+			}
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
