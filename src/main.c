eb93a011af483cd3165c2460a1ef2e32e874180e
Chris Muga <ChrisMuga94@Gmail.com>
2026-03-14 06:31:54 +0300

    refactor: update drawing to use touch/mouse positions with reset on backspace

--- src/main.c
+++ src/main.c
@@ -3,64 +3,75 @@
 
 #include "../libraries/raylib/src/raylib.h"
 
+int MAX = 100;
 
-int main(void)
-{
-    InitWindow(800, 450, "CIRCA");
-	// TODO: Cannot track the mouse on partial sized displays, i.e non-fullscreen displays
-	// SetWindowSize(1600, 1200);
+void drawPoints(Vector2 mousePositions[100], int *idx) {
+  Vector2 MousePosition = GetTouchPosition(0);
+  printf("----> <%f, %f>\n", MousePosition.x, MousePosition.y);
+  DrawCircle(MousePosition.x, MousePosition.y, 10.0, RED);
+  int maxObtained = *idx == MAX;
 
-	// NOTE: Mouse position tracking only works with fullscreen for now.
-	ToggleFullscreen();
+  if (*idx < MAX) {
+    if (MousePosition.x > 0) {
+      mousePositions[*idx] = MousePosition;
+      printf("IDX --> %d", *idx);
+      *idx += 1;
+    }
+  }
 
-	SetTargetFPS(60);
+  if (maxObtained) {
+    DrawText("MAX OBTAINED", 190, 300, 60, LIGHTGRAY);
 
-	int MAX = 100;
+    for (int i = 0; i < MAX; i++) {
+      Vector2 pos = mousePositions[i];
+      int py = 300 + (10 * i + 1);
+      DrawCircleV(pos, 10.0, RED);
+    }
+  }
+}
 
-	Vector2 mousePositions[100] = {0};
+void reset(Vector2 mousePositions[100], int *idx) {
+  for (int i = 0; i < MAX; i++) {
+    Vector2 v = {0, 0};
+    mousePositions[i] = v;
+  }
+  idx = 0;
+}
 
-	int idx = 0;
+int main(void) {
+  InitWindow(800, 450, "CIRCA");
+  // TODO: Cannot track the mouse on partial sized displays, i.e non-fullscreen
+  // displays SetWindowSize(1600, 1200);
 
+  // NOTE: Mouse position tracking only works with fullscreen for now.
+  ToggleFullscreen();
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-            ClearBackground(GRAY);
-			Vector2 MousePosition = GetMousePosition();
-			printf("----> <%f, %f>\n", MousePosition.x, MousePosition.y);
-			DrawCircle(MousePosition.x, MousePosition.y, 10.0, RED);
-			
-			bool mousePositionDown = IsMouseButtonDown(1);
-			bool mousePositionReleased = IsMouseButtonReleased(1);
-			bool maxObtained = idx == MAX;
+  SetTargetFPS(60);
 
+  Vector2 mousePositions[100] = {0};
+  int idx = 0;
 
-            DrawText("CIRCA", 190, 200, 60, LIGHTGRAY);
+  while (!WindowShouldClose()) {
+    bool mouseButtonDown = IsMouseButtonDown(0);
+    int keyPressed = GetKeyPressed();
 
-			if(idx < MAX){
-				if(MousePosition.x > 0){
-					mousePositions[idx] = MousePosition;
-					printf("IDX --> %d", idx);
-					idx += 1;
-				}
-			}
-
-			if(maxObtained) {
-				DrawText("MAX OBTAINED", 190, 300, 60, LIGHTGRAY);
-
-				for(int i = 0; i < MAX; i++){
-					Vector2 pos = mousePositions[i];
-					int py = 300 + (10 * i+1);
-					// DrawPixelV(pos, BLUE);
-					DrawCircleV(pos, 10.0, RED);
-					// Vector2 v = {0, 0};
-					// mousePositions[i] =  v;
-				}
-
-				// idx = 0;
-			}
-        EndDrawing();
+    if (keyPressed == KEY_BACKSPACE) {
+      reset(mousePositions, &idx);
     }
 
-    CloseWindow();
+    BeginDrawing();
+    ClearBackground(GRAY);
+    DrawText("CIRCA", 190, 200, 60, LIGHTGRAY);
+
+    if (mouseButtonDown) {
+      DrawText("DRAWING...", 190, 100, 60, LIGHTGRAY);
+      drawPoints(mousePositions, &idx);
+    } else {
+      idx = 0;
+    }
+
+    EndDrawing();
+  }
+
+  CloseWindow();
 }
