ed8facd363438836eac950ac9e7699bc5e51c0ef
Chris Muga <ChrisMuga94@Gmail.com>
2026-03-17 11:21:09 +0300

    feat: add color selection buttons with custom backgrounds and improved layout

--- src/main.c
+++ src/main.c
@@ -6,17 +6,32 @@
 #include "../libraries/raygui.h"
 
 #define MAX 10000
-void paintAll(Vector2 mousePositions[MAX]) {
+#define NUM_OF_COLORS 5
+
+struct ColorMap {
+	char name[10];
+	Color color;
+};
+
+struct ColorMap Colors[5] = {
+	{"Blue", BLUE},
+	{"Purple", PURPLE},
+	{"DarkGray", DARKGRAY},
+	{"Black", BLACK},
+	{"White", WHITE},
+};
+
+void paintAll(Vector2 mousePositions[MAX], Color color) {
   // Draw our "brush strokes"
   for (int i = 0; i < MAX; i++) {
     Vector2 pos = mousePositions[i];
     if (pos.x > 0 && pos.y > 0) {
-      DrawCircleV(pos, 10.0, GREEN);
+      DrawCircleV(pos, 10.0, color);
     }
   }
 }
 
-void drawPoints(Vector2 mousePositions[MAX], int *idx, bool mouseButtonDown) {
+void drawPoints(Vector2 mousePositions[MAX], int *idx, bool mouseButtonDown, Color color) {
   Vector2 MousePosition = GetTouchPosition(0);
 
   printf("----> <%f, %f>\n", MousePosition.x, MousePosition.y);
@@ -30,7 +45,7 @@ void drawPoints(Vector2 mousePositions[MAX], int *idx, bool mouseButtonDown) {
     }
   }
 
-  paintAll(mousePositions);
+  paintAll(mousePositions, color);
 }
 
 void reset(Vector2 mousePositions[MAX], int *idx) {
@@ -44,8 +59,9 @@ void reset(Vector2 mousePositions[MAX], int *idx) {
 int main(void) {
   InitWindow(1600, 1200, "CIRCA");
 
-  SetWindowState(FLAG_WINDOW_RESIZABLE);
-  MaximizeWindow();
+  ToggleFullscreen();
+  // SetWindowState(FLAG_WINDOW_RESIZABLE);
+  // MaximizeWindow();
 
   // NOTE: Mouse position tracking only works with fullscreen for now.
   // ToggleFullscreen(); // TODO: For some reason, partial dimensions or sub-max
@@ -56,6 +72,7 @@ int main(void) {
   Vector2 mousePositions[MAX] = {};
   int idx = 0;
   bool shouldClear = false;
+  Color currentColor = BLUE; // Default to blue
 
   while (!WindowShouldClose()) {
     bool mouseButtonDown = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
@@ -70,26 +87,36 @@ int main(void) {
 
     DrawText("CIRCA", 190, 200, 60, LIGHTGRAY);
     DrawText("Hold left click to start drawing...", 190, 250, 20, LIGHTGRAY);
+    DrawText("Select Color:", 190, 320, 20, LIGHTGRAY);
 
-    // TODO: How do we add icons to GuiButton?
-    if (GuiButton((Rectangle){190, 280, 200, 80}, "Clear")) {
+    // Clear button with neutral background
+    GuiSetStyle(BUTTON, BASE_COLOR_NORMAL, ColorToInt(LIGHTGRAY));
+    if (GuiButton((Rectangle){190, 350, 200, 50}, "Clear")) {
       shouldClear = true;
     }
+    GuiSetStyle(BUTTON, BASE_COLOR_NORMAL, ColorToInt(DARKGRAY)); // Reset
     GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
+
+    // Color selection buttons with adjusted positions and borders
+    for(int i = 0; i < NUM_OF_COLORS; i++){
+      GuiSetStyle(BUTTON, BASE_COLOR_NORMAL, ColorToInt(Colors[i].color));
+      GuiSetStyle(BUTTON, BORDER_COLOR_NORMAL, ColorToInt(BLACK)); // Black border for visibility
+      if (GuiButton((Rectangle){190, 410 + 60*i, 200, 50}, Colors[i].name)) {
+        currentColor = Colors[i].color;
+      }
+      GuiSetStyle(BUTTON, BASE_COLOR_NORMAL, ColorToInt(DARKGRAY));
+      GuiSetStyle(BUTTON, BORDER_COLOR_NORMAL, ColorToInt(GRAY)); // Reset border
+    }
 
     if (shouldClear) {
       reset(mousePositions, &idx);
       shouldClear = false;
     }
 
-    // TODO: When we stop holding the left click, we should find a way to simply
-    // draw the mouse positions without adding to mouse positions,
-    // - perhaps we will have to pass a boolean to denote "viewing mode", vs
-    // "drawing mode"
     if (mouseButtonDown) {
       DrawText("DRAWING...", 190, 100, 20, LIGHTGRAY);
     }
-    drawPoints(mousePositions, &idx, mouseButtonDown);
+    drawPoints(mousePositions, &idx, mouseButtonDown, currentColor);
 
     EndDrawing();
   }
